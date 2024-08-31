a3ad53a59a01b97d00cc0ec680bfd8386de418c2
chun <zzchun12826@gmail.com>
2024-09-01 01:25:45 +0800

    update

--- src/components/WithPermission.tsx
+++ src/components/WithPermission.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, useCallback, useMemo } from 'react';
-import { Modal, Form, Input, Button } from 'antd';
+import { ReactNode, useCallback, useEffect, useMemo } from 'react';
+import { Modal, Form, Input, Button, Space } from 'antd';
 import { useLocalStorageState } from 'ahooks';
-import { decrypt } from './Utils';
+import { decrypt, encrypt } from './Utils';
 
 interface Permission {
     name: string;
@@ -38,6 +38,12 @@ export function useResetLicense() {
 
 export function usePermission(key: string, msg: string = "尚未授权，请输入最新序列号") {
     const [license, setLicense] = useLocalStorageState<string | null>("license");
+    const [firstOpentime, setFirstOpentime] = useLocalStorageState<number | null>("firstOpentime");
+    useEffect(() => {
+        if (!firstOpentime) {
+            setFirstOpentime(Date.now());
+        }
+    }, [firstOpentime])
     const permission = useMemo<Permission>(() => {
         Modal.destroyAll();
         if (license) {
@@ -63,7 +69,24 @@ export function usePermission(key: string, msg: string = "尚未授权，请输�
                     <Input placeholder="请输入序列号" />
                 </Form.Item>
                 <Form.Item>
-                    <Button type="primary" htmlType="submit">确认</Button>
+                    <Space>
+                        <Button type="primary" htmlType="submit">确认</Button>
+                        {(function () {
+                            const _3days = 1000 * 60 * 60 * 24 * 3;
+                            if (Date.now() - firstOpentime! < _3days) {
+                                return <Button onClick={() => {
+                                    setLicense(encrypt(JSON.stringify({
+                                        baseddl: {
+                                            name: "使用授权",
+                                            type: "ddl",
+                                            value: firstOpentime! + _3days
+                                        }
+                                    })))
+                                }}>使用试用序列号</Button>;
+                            }
+                            return <></>;
+                        })()}
+                    </Space>
                 </Form.Item>
             </Form>,
             footer: null,
